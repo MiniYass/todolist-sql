f041c44ff1b57b336981b9c151bad7678b24846f
Sekri Yassine <58735130+MiniYass@users.noreply.github.com>
2020-03-19 15:13:55 +0100

    ajout de la todolist fonctionnel niveau ajout d'une tache , et archivage d'une tache 'a faire' tout ok pdo OK

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php 
+
+$dbHost="localhost";
+$dbuser="root";
+$dbpwd="";
+$dbname="todolist";
+
+try{
+    $dns="mysql:host=".$dbHost.";dbname=".$dbname.";charset=utf8";
+    $bdd= new PDO($dns,$dbuser,$dbpwd);
+
+    $table=$bdd->query("SELECT * FROM todo");
+    $architab=$bdd->query("SELECT * FROM done");
+    // ajout dans la liste a faire : 
+        if(isset($_POST['addtodo'])){
+           
+            $added=$_POST['addTache'];
+        
+            $tableAj = $bdd->prepare("INSERT INTO todo (todoName) VALUES (?)");
+            $tableAj->bindParam(1, $added);
+            $tableAj->execute();
+            echo "<script>alert('tache bien envoyé !');</script>";
+        }
+    // ajout dans la liste deja faite : 
+        if(isset($_POST['addDone'])){
+            // verifie l'id de la checkbox et va chercher les donnée dans la table correspondante
+            $idCheck=$_POST["checkDo"];
+
+             $checkedVal=$bdd->query("SELECT * FROM todo WHERE idTodo=$idCheck");
+                foreach ($checkedVal as $row){
+                    $toAdd=$row["todoName"];
+                }
+        
+            // insert la valeur dans la table des archive
+             $tableAr = $bdd->prepare("INSERT INTO done (doneName) VALUES (?)");
+             $tableAr->bindParam(1, $toAdd);
+             $tableAr->execute();
+           
+             // supprime de la bdd todo une fois l'info ajouter aux archiver
+             $sql="DELETE FROM todo WHERE idTodo=$idCheck";
+             $deleteResult=$bdd->prepare($sql);
+             $deleteResult->execute();
+        }
+
+
+}catch(PDOExeption $e){
+    echo "DB connexion ratey";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>To do list</title>
+</head>
+<body>
+
+<h3>A faire</h3>
+<form action="" method="post">
+<?php 
+    foreach ($table as $row){
+    
+        echo"<label for='checkDo'>".$row["todoName"]."</label>
+        <input type='checkbox' name='checkDo' id='checkDo' value=".$row["idTodo"]."> </br>";
+    }
+?>
+<input type="submit" name="addDone" value="enregistré">
+</form>
+
+
+<h3>archiver</h3>
+<?php 
+    foreach ($architab as $row){
+        echo"<label id='labDone' for='checkdone'>".$row["doneName"]."</label>
+        <input type='checkbox' name='checkdone' id='checkdone' checked> </br>";
+    }
+?>
+
+<h3>ajouter une tâche</h3>
+<form action="" method="post">
+    <label for="addTache">entrez une tâche</label>
+    <input type="textarea" name="addTache" id="addTache">
+
+    <input type="submit" name="addtodo" value="ajouter">
+</form>
+
+</body>
+</html>
